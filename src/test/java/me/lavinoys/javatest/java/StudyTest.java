55fb20f3bd2748bc32622a5496e36b393718eff5
jay.moon <jay.moon@kakaocorp.com>
2020-07-06 16:28:59 +0900

    assertAll

--- src/test/java/me/lavinoys/javatest/java/StudyTest.java
+++ src/test/java/me/lavinoys/javatest/java/StudyTest.java
@@ -13,15 +13,12 @@ class StudyTest {
     @Test
     @DisplayName("스터디 만들기")
     void create_new_study() {
-        Study study = new Study();
-        assertNotNull(study);
-        // 기대값, 동작, 에러 원인
-        assertEquals(StudyStatus.DRAFT, study.getStatus(), new Supplier<String>() {
-            @Override
-            public String get() {
-                return "스터디를 처음 만들면 DRAFT 상태다.";
-            }
-        });
+        Study study = new Study(0);
+        assertAll(
+                () -> assertNotNull(study),
+                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면" + StudyStatus.DRAFT +" 상태다."),
+                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 다능 인원은 0보다 커야 한다.")
+        );
     }
 
     @Test
